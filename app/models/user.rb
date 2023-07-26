aa06cb0c4bd1d664d434b48fc6ef5e076c109b54
TechMishima <t.mishima242@gmail.com>
2023-07-26 15:53:08 +0900

    (仮)バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname          , presence: true
+  validates :lastname          , presence: true
+  validates :firstname         , presence: true
+  validates :lastname_furigana , presence: true
+  validates :firstname_furigana, presence: true
+  validates :birthday          , presence: true
+
+  has_many :items
+  has_many :orders
 end

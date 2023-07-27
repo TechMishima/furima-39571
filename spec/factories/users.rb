1a472564cdea400f19d9c106ab2477a9421f13d5
TechMishima <t.mishima242@gmail.com>
2023-07-27 11:24:28 +0900

    userモデル 単体テストコードの作成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,16 @@
 FactoryBot.define do
   factory :user do
+    transient do
+      person { Gimei.name }
+    end
     nickname              {Faker::Name.initials(number: 2)}
     email                 {Faker::Internet.email}
     password              {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
-    lastname              {Faker::Name.last_name + "あア"}
-    firstname             {Faker::Name.first_name + "あア"}
-    lastname_furigana     {"ヤマダ"}
-    firstname_furigana    {"タロウ"}
+    lastname              { person.last.kanji + "あア"}
+    firstname             { person.first.kanji + "あア"}
+    lastname_furigana     { person.last.katakana }
+    firstname_furigana    { person.first.katakana }
     birthday              {Faker::Date.birthday(min_age: 6, max_age: 92)}
   end
 end

f1916cbf0ce17d4e72d554555e126a86673ba9df
TechMishima <t.mishima242@gmail.com>
2023-07-28 16:37:54 +0900

    renderの設定、ログイン有無のリダイレクト

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, only: [:new]
+
   def index
   end
 
@@ -11,13 +13,19 @@ class ItemsController < ApplicationController
     if @item.save
       redirect_to '/'
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
   private
   def item_params
     params.require(:item).permit(:name, :description, :category_id, :status_id, :delivery_cost_id, :prefecture_id, :shipping_day_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 
 end

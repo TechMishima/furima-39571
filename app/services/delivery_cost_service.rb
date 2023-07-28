52e775055b114ce6e89afbaa5f5ea338ed77a225
TechMishima <t.mishima242@gmail.com>
2023-07-28 11:16:49 +0900

    アクティブハッシュのサービスクラスを作成

---
+++ app/services/delivery_cost_service.rb
@@ -0,0 +1,9 @@
+class DeliveryCostService
+  def self.options
+    [
+      { id: 1, delivery_cost: '---' },
+      { id: 2, delivery_cost: '着払い(購入者負担)' },
+      { id: 3, delivery_cost: '送料込み(出品者負担)' },
+    ]
+  end
+end

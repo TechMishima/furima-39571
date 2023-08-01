d92dded0091fb76a9d5d1943b6143bd2a747ca9b
TechMishima <t.mishima242@gmail.com>
2023-08-01 15:33:19 +0900

    Formオブジェクトでのデータ保存実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,13 +7,14 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
+    # binding.pry
     if @order_address.valid?
       pay_item
       @order_address.save
       redirect_to root_path
     else
-      @item = Item.find(params[:item_id])
       render 'index', status: :unprocessable_entity
     end
   end
@@ -22,14 +23,14 @@ class OrdersController < ApplicationController
 
    def order_params
      params.require(:order_address).permit(
-      :user_id, :item_id, :post_code, :prefecture_id, :shipping_municipality, :shipping_address, :shipping_building, :buyer_phone_number, :order_id)
-      .merge(token: params[:token])
+      :user_id, :post_code, :prefecture_id, :shipping_municipality, :shipping_address, :shipping_building, :buyer_phone_number)
+      .merge(token: params[:token], user_id: current_user.id, item_id: @item.id)
    end
 
   def pay_item
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
-    amount: order_params[:price],  # 商品の値段
+    amount: @item.price,  # 商品の値段
     card: order_params[:token],    # カードトークン
     currency: 'jpy'                 # 通貨の種類（日本円）
   )

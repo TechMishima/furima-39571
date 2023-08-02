4eddf02cc0bff8d8c3f9483e818554ba990fee04
TechMishima <t.mishima242@gmail.com>
2023-08-02 17:52:16 +0900

    修正２

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -24,7 +24,7 @@ RSpec.describe OrderAddress, type: :model do
       expect(@order_address.errors.full_messages).to include("Post code can't be blank")
     end
     it 'post_codeが7桁の数字だと購入できない' do
-      @order_address.post_code = 1234567
+      @order_address.post_code = `1234567`
       @order_address.valid?
       expect(@order_address.errors.full_messages).to include('Post code is invalid')
     end
@@ -49,22 +49,22 @@ RSpec.describe OrderAddress, type: :model do
       expect(@order_address.errors.full_messages).to include("Buyer phone number can't be blank")
     end
     it 'buyer_phone_numberが9桁の数字だと購入できない' do
-      @order_address.buyer_phone_number = 123456789
+      @order_address.buyer_phone_number = `123456789`
       @order_address.valid?
       expect(@order_address.errors.full_messages).to include("Buyer phone number is invalid")
     end
     it 'buyer_phone_numberが12桁の数字だと購入できない' do
-      @order_address.buyer_phone_number = 123456789012
+      @order_address.buyer_phone_number = `123456789012`
       @order_address.valid?
       expect(@order_address.errors.full_messages).to include("Buyer phone number is invalid")
     end
     it 'buyer_phone_numberが3桁-4桁-4桁で入力しても購入できない' do
-      @order_address.buyer_phone_number = 123-1234-1234
+      @order_address.buyer_phone_number = `123-1234-1234`
       @order_address.valid?
       expect(@order_address.errors.full_messages).to include("Buyer phone number is invalid")
     end
     it '都道府県に「---」が選択されている場合は購入できない' do
-      @order_address.prefecture_id = 1
+      @order_address.prefecture_id = `1`
       @order_address.valid?
       expect(@order_address.errors.full_messages).to include("Prefecture can't be blank")
     end

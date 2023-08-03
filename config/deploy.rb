052231f01ad60b1d84d7ae39f21a9fcebdfcbc4f
TechMishima <t.mishima242@gmail.com>
2023-08-03 22:13:53 +0900

    自動デプロイ

---
+++ config/deploy.rb
@@ -0,0 +1,26 @@
+lock "~> 3.17.3"
+
+set :application, "furima-39571"
+set :repo_url, "https://github.com/TechMishima/furima-39571.git"
+
+set :branch, 'main'
+
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '3.2.0'
+
+set :ssh_options, auth_methods: ['publickey'],
+                                  keys: ['~/.ssh/key.pem'] 
+
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
